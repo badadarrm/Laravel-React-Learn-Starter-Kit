89ef25a000f234681883ccc611100cf9f07f894f
badadarr <badar.maulana.techno@gmail.com>
2025-11-05 10:20:05 +0700

    Add rel="noopener" to external links in Welcome component for security

--- lara-react-crud/resources/js/pages/welcome.tsx
+++ lara-react-crud/resources/js/pages/welcome.tsx
@@ -71,6 +71,7 @@ export default function Welcome({
                                         <a
                                             href="https://laravel.com/docs"
                                             target="_blank"
+                                            rel="noopener"
                                             className="ml-1 inline-flex items-center space-x-1 font-medium text-[#f53003] underline underline-offset-4 dark:text-[#FF4433]"
                                         >
                                             <span>Documentation</span>
@@ -102,6 +103,7 @@ export default function Welcome({
                                         <a
                                             href="https://laracasts.com"
                                             target="_blank"
+                                            rel="noopener"
                                             className="ml-1 inline-flex items-center space-x-1 font-medium text-[#f53003] underline underline-offset-4 dark:text-[#FF4433]"
                                         >
                                             <span>Laracasts</span>
@@ -128,6 +130,7 @@ export default function Welcome({
                                     <a
                                         href="https://cloud.laravel.com"
                                         target="_blank"
+                                        rel="noopener"
                                         className="inline-block rounded-sm border border-black bg-[#1b1b18] px-5 py-1.5 text-sm leading-normal text-white hover:border-black hover:bg-black dark:border-[#eeeeec] dark:bg-[#eeeeec] dark:text-[#1C1C1A] dark:hover:border-white dark:hover:bg-white"
                                     >
                                         Deploy now
